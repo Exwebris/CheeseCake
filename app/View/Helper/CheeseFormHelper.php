8f61114db2dcb97ba0043d735eb26b1c55d76906
Leonid Mamchenkov <leonidm@easy-forex.com>
2014-01-09 12:27:22 +0200

    Use datepicker or datetimepicker based on dateFormat

--- app/View/Helper/CheeseFormHelper.php
+++ app/View/Helper/CheeseFormHelper.php
@@ -5,7 +5,18 @@ App::uses('FormHelper', 'View/Helper');
 class CheeseFormHelper extends FormHelper {
 
 	public function dateTime($fieldName, $dateFormat = 'DMY', $timeFormat = '12', $attributes = array()) {
-		return $this->input($fieldName, array('type' => 'text', 'class' => 'datetimepicker'));
+		$class = 'datepicker';
+		
+		// See if we need to use time picker as well
+		$dateFormatChars = str_split($dateFormat);
+		$timeChars = array('a', 'A', 'B', 'g', 'G', 'h', 'H', 'i', 's', 'u'); // based on date()
+		foreach ($timeChars as $timeChar) {
+			if (in_array($timeChar, $dateFormatChars)) {
+				$class = 'datetimepicker';
+				break;
+			}
+		}
+		return $this->input($fieldName, array('type' => 'text', 'class' => $class));
 	}
 }
 ?>
